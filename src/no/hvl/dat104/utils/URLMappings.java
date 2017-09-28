cb38f60e26237ae8bfb72f7c15ae21778d9c2a61
Adne <aadne8@gmail.com>
2017-09-28 16:25:46 +0200

    Added URLMapping

--- src/no/hvl/dat104/utils/URLMappings.java
+++ src/no/hvl/dat104/utils/URLMappings.java
@@ -6,4 +6,6 @@ public class URLMappings {
 	public static final String LOGIN_JSP_URL = "/WEB-INF/jsp/login.jsp";
 	public static final String REGISTER_JSP_URL = "/WEB-INF/jsp/registration/registerform.jsp";
 	public static final String REGISTER_CONF_JSP_URL = "/WEB-INF/jsp/registration/registerconfirmation.jsp";
+	public static final String USERLIST_URL = "/WEB-INF/jsp/user/userlist.jsp";
+	
 }
