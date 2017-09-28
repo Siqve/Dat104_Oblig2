6726772c1a4bad6ef5958dbd5d9fe5d73695f180
Sondre <h152078@stud.hib.no>
2017-09-28 14:31:08 +0200

    Basis for RegisterServlet.

---
+++ src/no/hvl/dat104/servlets/RegisterServlet.java
@@ -0,0 +1,33 @@
+package no.hvl.dat104.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import no.hvl.dat104.utils.URLMappings;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Forward to registerform.jsp
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(URLMappings.REGISTER_JSP_URL);
+		dispatcher.forward(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/no/hvl/dat104/utils/URLMappings.java
+++ src/no/hvl/dat104/utils/URLMappings.java
@@ -4,6 +4,7 @@ public class URLMappings {
 	
 	public static final String LOGIN_URL = "login";
 	public static final String LOGIN_JSP_URL = "WEB-INF/jsp/login.jsp";
+	public static final String REGISTER_JSP_URL = "WEB-INF/jsp/registerform.jsp";
 	
 	
 
